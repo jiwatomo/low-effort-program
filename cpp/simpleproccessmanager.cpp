611400a98303b49279a33f3f7a08164109bfb41d
jiwatomo <jiwatomo@protonmail.com>
2021-08-31 21:01:30 +0700

    try std::format

--- cpp/simpleproccessmanager.cpp
+++ cpp/simpleproccessmanager.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 #include <string>
+#include <format>
 #include <conio.h>
 using namespace std;
 
 int main() {
 	while (1) {
 		string mainmenu = "Proccess Manager\n1.See all proccess\n2.Kill specific proccess\ntype any to exit";
-		cout<<mainmenu<<endl
+		cout<<mainmenu<<endl;
 		char getmain = _getch();
 		if (getmain == '1') {
 			system("tasklist");
 		} else if (getmain == '2') {
 			int pidhandler;
+			string commandtemp = format("taskkill /F /PID {} /T", pidhandler);
 			cin>>pidhandler;
-			system("taskkill /F /PID %z", pidhandler)
+			system(commandtemp);
 		} else {
 			break;
 		}
