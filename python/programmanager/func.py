4b58053eb55f71c6b9127f8650d8bbabc008235c
jiwatomo <jiwatomo@protonmail.com>
2021-12-26 21:26:55 +0700

    Top, Mid done, function still not

--- python/programmanager/func.py
+++ python/programmanager/func.py
@@ -3,4 +3,21 @@ def check():
 	
 	
 def mid_button_handler(val1):
-	print(val1)
+	if val1 == "test1":
+		print("Done")
+	elif val1 == "test2":
+		print("Done")
+	elif val1 == "test3":
+		print("Done")
+	elif val1 == "test4":
+		print("Done")
+	elif val1 == "test5":
+		print("Done")
+	elif val1 == "test6":
+		print("Done")
+	elif val1 == "test7":
+		print("Done")
+	elif val1 == "test8":
+		print("Done")
+	elif val1 == "test9":
+		print("Done")
--- python/programmanager/main.py
+++ python/programmanager/main.py
@@ -15,20 +15,22 @@ def main():
 	top_area.pack()
 	dlabel = tk.Label(master=top_area, text="Welcome!, select the mode first")
 	dlabel.pack(side=tk.LEFT)
-	tfr = tk.Frame(master=top_area, width="200")
+	tfr = tk.Frame(master=top_area, width="33")
 	tfr.pack(side=tk.LEFT, fill=tk.X)
 	btn_mode = tk.Button(master=top_area, text="Change mode")
 	btn_mode.pack(side=tk.RIGHT)
 	
 	
 	# Mid
-	mid_area = tk.Frame(relief=tk.SUNKEN)
+	mid_area = tk.Frame(relief=tk.SUNKEN, borderwidth=4)
 	mid_area.pack()
 	#  dlabel_mid = tk.Label(master=mid_area, text="Mid Area")
 	#  dlabel_mid.pack()
+	
 	# Side left
-	left_side =tk.Frame(master=mid_area)
+	left_side =tk.Frame(master=mid_area, borderwidth=3)
 	left_side.pack(fill=tk.Y, side=tk.LEFT)
+	
 	btn_perform = tk.Button(master=left_side, text="Performance")
 	btn_perform.pack(fill=tk.X)
 	btn_ExplorerON = tk.Button(master=left_side, text="ExplorerON")
@@ -36,8 +38,16 @@ def main():
 	btn_ExplorerOFF = tk.Button(master=left_side, text="ExplorerOFF")
 	btn_ExplorerOFF.pack(fill=tk.X)
 	
+	frame_kosong_config = {
+		"widtha": 20,
+		"heighta": 1
+	}
+	
+	frame_kosong = tk.Frame(master=mid_area, width=frame_kosong_config["widtha"], height=frame_kosong_config["heighta"])
+	frame_kosong.pack(side=tk.LEFT,fill=tk.X)
+	
 	# Center
-	center_side = tk.Frame(master=mid_area)
+	center_side = tk.Frame(master=mid_area, borderwidth=3)
 	center_side.pack(fill=tk.Y, side=tk.LEFT)
 	
 	btn_list_row1 = ["test1", "test2", "test3"]
@@ -68,10 +78,20 @@ def main():
 				btn_mid = tk.Button(master=btnframe, text=j, command=partial(mid_button_handler, j))
 				btn_mid.pack()
 				inc += 1
+		
+	frame_kosong = tk.Frame(master=mid_area, width=frame_kosong_config["widtha"], height=frame_kosong_config["heighta"])
+	frame_kosong.pack(side=tk.LEFT,fill=tk.X)
 				
 	# Side right
-	right_side = tk.Frame(master=mid_area)
-	right_side.pack(fill=tk.Y, side=tk.LEFT)
+	left_side = tk.Frame(master=mid_area, borderwidth=3)
+	left_side.pack(fill=tk.Y, side=tk.RIGHT)
+	
+	btn_command = tk.Button(master=left_side, text="cmd")
+	btn_command.pack(fill=tk.X)
+	btn_kill = tk.Button(master=left_side, text="Kill")
+	btn_kill.pack(fill=tk.X)
+	btn_exit = tk.Button(master=left_side, text="Exit")
+	btn_exit.pack(fill=tk.X)
 	
 	
 	# Bottom
